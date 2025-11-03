aa7c910ef02ce0d7a94275e0ea12217a355541d6
xuxx21 <ceciliaxu02@gmail.com>
2025-11-04 00:55:46 +0800

    更新延迟问题.js

--- P5_Interface1D/player.js
+++ P5_Interface1D/player.js
@@ -1,30 +1,31 @@
-
-// This holds some player information, like color and position.
-// It also has some player methods for managing how a player moves.
-
-
 class Player {
-  
-    constructor(_color, _position, _displaySize) {
-        this.playerColor = _color;
-        this.position = _position;
-        this.score = 0;
-        this.displaySize = _displaySize;
-    }
 
+  constructor(_color, _position, _displaySize) {
+    this.playerColor = _color;
+    this.position = _position;
+    this.score = 0;
+    this.displaySize = _displaySize;
 
-    // Move player based on keyboard input
-    move(_direction) {
-
-        // increments or decrements player position
-        this.position = this.position + _direction;
-      
-        // if player hits the edge of display, loop around
-        if (this.position == -1) {
-            this.position = this.displaySize - 1;
-        } else if (this.position == this.displaySize) {
-            this.position = 0;
-        } 
-         
-    } 
+    this.speed = 200; // degrees per second（控制移动速度，可调大一点更快）
   }
+
+  // 每帧更新位置（连续移动）
+  updateFromKeyboard(leftKey, rightKey) {
+    const stepPerDeg = this.displaySize / 360;
+    let deltaDeg = 0;
+
+    // 每帧按键检测
+    if (keyIsDown(leftKey))  deltaDeg -= this.speed * (deltaTime / 1000);
+    if (keyIsDown(rightKey)) deltaDeg += this.speed * (deltaTime / 1000);
+
+    // 累加
+    this.position = (this.position + deltaDeg * stepPerDeg + this.displaySize) % this.displaySize;
+  }
+
+  // 如果仍要支持老的单步 move()（例如硬件按一下）
+  move(_direction) {
+    this.position += _direction;
+    if (this.position < 0) this.position = this.displaySize - 1;
+    if (this.position >= this.displaySize) this.position = 0;
+  }
+}
